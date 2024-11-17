530b46969596f29b7f84720462cbcf0eb3f8b927
Bertalan Körmendy <korber314@gmail.com>
2024-11-17 11:31:09 +0100

    d2

---
+++ src/02/02.ts
@@ -0,0 +1,69 @@
+import { product, sum } from '@/advent'
+
+const Colors = ['red', 'green', 'blue'] as const
+type Color = (typeof Colors)[number]
+
+interface Cube {
+  color: Color
+  amount: number
+}
+
+interface Game {
+  cubes: Cube[]
+}
+
+export function parse(input: string): Game[] {
+  const isColor = (color: any): color is Color => Colors.includes(color)
+
+  const pDraw = (raw: string) =>
+    raw.split(', ').map(c => {
+      const [amount, color] = c.split(' ')
+      if (!isColor(color)) {
+        throw new Error(`Invalid color: ${color}`)
+      }
+
+      return { color, amount: parseInt(amount!) }
+    })
+
+  return input
+    .split('\n')
+    .filter(l => l.length > 0)
+    .map(l => {
+      const [_, cubesStr] = l.split(': ')
+      const cubes = cubesStr!.split('; ').flatMap(pDraw)
+      return { cubes }
+    })
+}
+
+type Input = ReturnType<typeof parse>
+
+export function partOne(input: Input) {
+  const limits = { red: 12, green: 13, blue: 14 }
+  return sum(
+    input.flatMap((game, id) =>
+      game.cubes.every(cube => cube.amount <= limits[cube.color])
+        ? [id + 1]
+        : []
+    )
+  )
+}
+
+interface Fewest {
+  red: number | null
+  green: number | null
+  blue: number | null
+}
+
+export function partTwo(input: Input) {
+  return sum(
+    input.map(game => {
+      let fewest: Fewest = { red: null, green: null, blue: null }
+      for (const cube of game.cubes) {
+        const amount = fewest[cube.color]
+        fewest[cube.color] =
+          amount == null ? cube.amount : Math.max(amount, cube.amount)
+      }
+      return product([fewest.red ?? 1, fewest.green ?? 1, fewest.blue ?? 1])
+    })
+  )
+}
--- src/advent.ts
+++ src/advent.ts
@@ -1,4 +1,5 @@
 export const sum = (ns: number[]) => ns.reduce((a, b) => a + b, 0)
+export const product = (ns: number[]) => ns.reduce((a, b) => a * b, 1)
 
 export function* tails(line: string) {
   for (let i = 0; i < line.length; i++) {
